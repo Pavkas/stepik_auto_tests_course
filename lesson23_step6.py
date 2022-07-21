03a03be8d7f65154f080ecfa1ca6bf2c93b3d626
Pavkas <new-adler@mail.ru>
2022-07-21 10:27:10 +0300

    swith to new tab

---
+++ lesson23_step6.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time, math
+
+url = 'https://suninjuly.github.io/redirect_accept.html'
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(url)
+    browser.find_element(By.TAG_NAME, 'button').click()
+# переходим на новую вкладку [1]
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, '#input_value').text
+    y = calc(x_element)
+    browser.find_element(By.CSS_SELECTOR, '#answer').send_keys(y)
+    browser.find_element(By.TAG_NAME, 'button').click()
+
+
+
+finally:
+	time.sleep(10)
+	browser.quit()
